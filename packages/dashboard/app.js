6fec49e79d571ca160d1ad46d6bd70b71e594cd3
bannmoore <moore.brittanyann@gmail.com>
2018-05-09 18:54:36 -0400

    feat: Add build script for dashboard package.

--- packages/dashboard/app.js
+++ packages/dashboard/app.js
@@ -4,8 +4,8 @@ const path = require('path')
 const app = express()
 
 app.use('/', express.static(path.resolve(__dirname)))
-app.use('/react-app-1', express.static(path.resolve(__dirname, 'node_modules/@bam/react-app-1/build')))
-app.use('/react-app-2', express.static(path.resolve(__dirname, 'node_modules/@bam/react-app-2/build')))
+app.use('/react-app-1', express.static(path.resolve(__dirname, 'public/react-app-1/build')))
+app.use('/react-app-2', express.static(path.resolve(__dirname, 'public/react-app-2/build')))
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
